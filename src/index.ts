cdbd490e62cfb09d0605e9c3a099b89b5ba1c587
yj.gaia <gaiaprotocolnft@gmail.com>
2025-08-12 23:12:32 +0900

    Refactor NftApiWorker to extend WorkerEntrypoint and use 'this.env' for context

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import { preflightResponse } from "@gaiaprotocol/worker-common";
+import { WorkerEntrypoint } from "cloudflare:workers";
 import { handleHeldNftsRequest } from "./handlers/held-nfts";
 import { handleMetadataRequest } from "./handlers/metadata";
 import { handleNftDataRequest } from "./handlers/nft";
 import { handleNftDataByIds } from "./handlers/nft-by-ids";
+import { fetchNftDataByIds } from "./services/nft";
 
-export default {
-  async fetch(request, env, ctx): Promise<Response> {
+export default class NftApiWorker extends WorkerEntrypoint<Env> {
+  async fetch(request: Request): Promise<Response> {
     if (request.method === 'OPTIONS') return preflightResponse();
 
     const url = new URL(request.url);
-    if (url.pathname.startsWith('/metadata/')) return handleMetadataRequest(request, env);
-    if (url.pathname.startsWith('/nft/')) return handleNftDataRequest(request, env);
-    if (url.pathname.endsWith('/nfts')) return handleHeldNftsRequest(request, env);
-    if (url.pathname.startsWith('/nfts/by-ids')) return handleNftDataByIds(request, env);
+    if (url.pathname.startsWith('/metadata/')) return handleMetadataRequest(request, this.env);
+    if (url.pathname.startsWith('/nft/')) return handleNftDataRequest(request, this.env);
+    if (url.pathname.endsWith('/nfts')) return handleHeldNftsRequest(request, this.env);
+    if (url.pathname === '/nfts/by-ids') return handleNftDataByIds(request, this.env);
 
     return new Response('Not Found', { status: 404 });
-  },
-} satisfies ExportedHandler<Env>;
+  };
+
+  fetchNftDataByIds(ids: string[]): Promise<Record<string, any>> {
+    return fetchNftDataByIds(this.env, ids)
+  }
+}
